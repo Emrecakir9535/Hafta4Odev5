d17ab665331662c74243344e31c4e17eb14efad9
Emre ÇAKIR <emrecakir9535@gmail.com>
2021-01-23 18:25:02 +0300

    Add project files.

---
+++ Hafta4Odev5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hafta4Odev5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(0, "Emre");
+            myDictionary.Add(1, "Engin");
+            myDictionary.Add(2, "Halil");
+            myDictionary.Add(10, "Yavuz");
+         
+
+        }
+    }
+}
